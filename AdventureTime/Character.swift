7225892a1594c7b353be2426c8cb209d1f783b3a
Taniraw <tanirawrites@gmail.com>
2016-10-06 17:03:20 -0400

    Ermagawd Adventure Time

--- AdventureTime/Character.swift
+++ AdventureTime/Character.swift
@@ -9,5 +9,67 @@
 import Foundation
 import UIKit
 
+//3 Create a mutating func called fight(enemy:). The parameter is of type Character. In your implementation of this function, you should check to see if the powerLevel (on the current instance) is greater than the enemies power level which is passed into this method as an argument. If it is then you should increment the killedEnemies stored property by 1. As well, you should increase the powerLevel stored property by 10.
+//
+//Within this same function, if the powerLevel of the current instance is less than the enemies powerLevel than you should decrease the powerLevel stored property by 10.
+//
+//4 Create a mutating func called becomeBestFriends(with:). It should take in one argument called friend of type String. In your implementation of this function, you should change your bestFriend (which is a stored property) to equal this new friend which was passed into this method as an argument.
+//
+//5 Copy & Paste this function below where you created your becomeBestFriends(with:) function. This will be its own new function which will come in handy later.
+
+
+
 
    
+struct Character{
+    
+    var name: String
+    var species: String
+    var occupation: String
+    var powerLevel: Double
+    var killedEnemies: Int = 0
+    var bestFriend: String
+    
+    
+  init(name:String, species:String, occupation:String, powerLevel: Double){
+        self.name = name
+        self.species = species
+        self.occupation = occupation
+        self.powerLevel = powerLevel
+        self.bestFriend = " "
+    }
+    
+    mutating func fight(_ enemy: Character){
+        if self.powerLevel  > enemy.powerLevel {
+            killedEnemies += 1
+            powerLevel += 10.0
+        } else {
+            powerLevel -= 10.0
+    }
+    }
+        mutating func becomeBestFriends(with friend:String) {
+            bestFriend = friend
+        }
+    func displayImage() -> UIImage {
+        
+        switch name {
+        case "Finn The Human":
+            return UIImage(named: "Finn")!
+        case "Jake The Dog":
+            return UIImage(named: "JakeTheDog")!
+        case "Princess Bubblegum":
+            return UIImage(named: "PrincessBubblegum")!
+        case "Lemongrab":
+            return UIImage(named: "Lemongrab")!
+        case "BMO":
+            return UIImage(named: "BMO")!
+        case "Lumpy Space Princess":
+            return UIImage(named: "LumpySpacePrincess")!
+        default:
+            return UIImage()
+        }
+        
+    }
+    
+
+}
--- AdventureTime/ViewController.swift
+++ AdventureTime/ViewController.swift
@@ -10,16 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    
+    @IBOutlet weak var characterImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var occupationLabel: UILabel!
+    @IBOutlet weak var speciesLabel: UILabel!
+    
+    var finn : Character!
+    var jake : Character!
+    var bubblegum : Character!
+    var bmo : Character!
+    var lemongrab : Character!
+    var lsp : Character!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        createAllCharacters()
     }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if let label = sender.titleLabel?.text! {
+            switch label{
+            case "Finn":
+                updateViews(with: finn)
+            case "Jake":
+                updateViews(with:jake)
+            case "Bubblegum":
+                updateViews(with: bubblegum)
+            case "BMO":
+                updateViews(with: bmo)
+            case "Lemongrab":
+                updateViews(with: lemongrab)
+            case "LSP":
+                updateViews(with: lsp)
+                
+            default: "Does not compute."
+                
+            }
+        }
+    }
+    
 
+    
+    
+    
+    func  createAllCharacters(){
+        finn = Character(name: "Finn The Human", species: "Human", occupation: "Hero", powerLevel: 50.0)
+        jake = Character (name: "Jake The Dog", species: "Magic Dog", occupation: "Sidekick", powerLevel: 70.0)
+        bubblegum = Character(name: "Princess Bubblegum", species: "Candy Person", occupation: "Princess of Candy Kingdom", powerLevel: 90.0)
+        bmo = Character(name: "BMO", species: "Robot Pal", occupation: "Caretaker", powerLevel: 10.0)
+        lemongrab = Character(name:"Lemongrab", species: "Lemon", occupation: "Earl of Lemongrab", powerLevel: 20.0)
+        lsp = Character(name: "Lumpy Space Princess", species: "Lumpy", occupation: "Princess of LumpySpace Kingdom", powerLevel: 30.0)
+    }
+    
+    func updateViews(with character: Character){
+        characterImageView.image = character.displayImage()
+        nameLabel.text = character.name
+        occupationLabel.text = character.occupation
+        speciesLabel.text = character.species
   
+    }
     
-    
-   
-
 }
+
+
+
 
